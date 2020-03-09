6da32db0f417da35fb76fd28bd98d30982c3d4ca
Vedant Patwari <vedantpatwari01@gmail.com>
2020-03-09 16:33:25 -0500

    Basic Movement
    Added basic ship movement

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int boundX, boundY;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        Vector3 inputV = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+        transform.position += inputV;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -boundX, boundX), Mathf.Clamp(transform.position.y, -boundY, boundY), 0);
+     
+    
+    }
+
+    
+
+    
+}
