9e09a120ebd7825a001b04d9bd0eefbbc87bce4d
Vedant Patwari <vedantpatwari01@gmail.com>
2020-03-10 23:45:25 -0500

    Advanced Movement
    - Ship moves more 'naturally'
    - ship tilts with movement

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -6,6 +6,9 @@ public class ShipController : MonoBehaviour
 {
     // Start is called before the first frame update
     public int boundX, boundY;
+    public int maxRoationDeg = 30;
+    public int maxVerticalRotation = 5;
+    public float rotationSpeed = 0.05f;
 
     void Start()
     {
@@ -19,10 +22,35 @@ public class ShipController : MonoBehaviour
 
         Vector3 inputV = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 
-        transform.position += inputV;
+        if (Mathf.Abs(inputV.x) <= 0.5)
+        {
+            inputV.x = -transform.GetComponent<Rigidbody>().velocity.x;
+        }
+        
+        if(Mathf.Abs(inputV.y) <= 0.5) {
+            inputV.y = -transform.GetComponent<Rigidbody>().velocity.y;
+        }
+
+        transform.GetComponent<Rigidbody>().AddForce(inputV);
+
 
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -boundX, boundX), Mathf.Clamp(transform.position.y, -boundY, boundY), 0);
-     
+
+        Vector3 rotation = new Vector3(Mathf.Lerp(0, maxVerticalRotation, Mathf.Abs(transform.GetComponent<Rigidbody>().velocity.y) * rotationSpeed), 0, Mathf.Lerp(0, maxRoationDeg, Mathf.Abs(transform.GetComponent<Rigidbody>().velocity.x)*rotationSpeed));
+
+        if (transform.GetComponent<Rigidbody>().velocity.x < 0) {
+            rotation.z = -rotation.z;
+        }
+
+        if (transform.GetComponent<Rigidbody>().velocity.y < 0) {
+            rotation.x = -rotation.x;
+        }
+
+
+        Debug.Log(rotation);
+
+
+        transform.rotation = Quaternion.Euler(rotation);
     
     }
 
