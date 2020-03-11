4ccf1f475dabd45efc0597250596eb60bbd80d01
Corey Tolbert <ctolbert555@gmail.com>
2020-03-11 12:06:24 -0500

    Added forward motion of space
    Added forward motion of space

---
+++ Assets/Scripts/SpaceMotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceMotion : MonoBehaviour
+{
+
+    public float game_duration; //in minutes
+    float speed;
+    Rigidbody myRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = -700 / (game_duration * 60);
+        myRb = GetComponent<Rigidbody>();
+        myRb.velocity = new Vector3(0, 0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRb.velocity = new Vector3(0, 0, speed);
+    }
+}
